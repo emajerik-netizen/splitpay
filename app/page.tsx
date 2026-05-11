0fdbca1f3abd9b8c9a8c2db91b17c8fef9d769b4
eugenmajerik-ACE <eugen.majerik@asseco-ce.cloud>
2026-05-11 11:54:44 +0200

    feat(members): createTrip initializes members as Member objects

--- app/page.tsx
+++ app/page.tsx
@@ -1088,7 +1088,7 @@ function createTrip(name: string, date: string, inviteCode: string, owner: strin
     inviteCode,
     deletedAt: null,
     deletedBy: null,
-    members: ['Ty'],
+    members: [{ id: null, name: 'Ty' }],
     expenses: [],
     pendingInvites: [],
   };
